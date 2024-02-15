f37d7b3a221436151e313eb2127a01f37f924017
joymajumder23 <joymajumderjishu@gmail.com>
2024-02-15 14:16:04 +0600

    sections added and js adding

---
+++ js/alpha-clash.js
@@ -0,0 +1,6 @@
+function play(){
+    // console.log('Connected');
+    const homeScreen = document.getElementById('home-screen')
+    // console.log(homeScreen.classList);
+    homeScreen.classList.add('hidden');
+}
